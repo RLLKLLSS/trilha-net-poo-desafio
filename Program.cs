797b5810e1de293197d77d3711297c5bee895dbc
=Romullo <=romanhang23@proton.me>
2024-01-01 20:33:58 -0300

    DESAFIO FEITO!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Iphone");
+Iphone iphone = new Iphone("231313","modelo1","sdfdsf545665",128);
+iphone.Ligar();
+iphone.InstalarAplicativo("Linkedin");
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("231313","modelo1","sdfdsf545665",128);
+nokia.Ligar();
+nokia.InstalarAplicativo("Signal");
